e12ac00f107477e0e246f831c5c1eaee36256807
Franklin <frkmrd@hotmail.com>
2015-08-18 19:22:32 -0400

    Primer Commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title></title>
+	<link rel="stylesheet" href="css/estilos_in.css">
+</head>
+<body>
+	<div id="cabezera">
+		<div id="nombre"></div>
+		<div id="slider"></div>
+		<div id="menu"></div>
+	</div>
+	<div id="cuerpo"></div>
+	<div id="pie"></div>
+	<div id="mapa"></div>
+</body>
+</html>
